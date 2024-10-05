79723fa172b1b3e95d8df1b2480249e4df600684
theopnv <theo.penavaire@gmail.com>
2024-10-05 21:28:20 +0200

    Update defaultBranch

--- vercel-submodule-workaround.sh
+++ vercel-submodule-workaround.sh
@@ -26,6 +26,7 @@ cd tmp # go into the tmp folder
 
 # checkout the current submodule commit
 git init # initialise empty repo
+git config --global init.defaultBranch main
 git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
 git fetch --depth=1 origin $COMMIT # fetch only the required version
 git checkout $COMMIT # checkout on the right commit
